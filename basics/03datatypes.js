d6792f484c6c94b22b05816cf859447a1d1845d1
adak08 <adarshkumarpbh1@gmail.com>
2025-06-09 06:39:35 +0000

    datatypes

---
+++ basics/03datatypes.js
@@ -0,0 +1,21 @@
+"use strict";// treat all JS code as newer version
+
+//alert(3 + 3) // we are using node js , not browser
+
+let name = "adarsh"
+let age = 19
+let isloogedin = false
+
+//number => 2^53
+//bigint
+// string => ""
+//boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object;
+
+console.log(typeof age);
+console.log(typeof null);// object
+console.log(typeof undefined);
