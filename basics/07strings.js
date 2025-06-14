c052e6e6d547290b359ce167af4557b6b3a9acf6
adak08 <adarshkumarpbh1@gmail.com>
2025-06-14 06:53:54 +0000

    strings

---
+++ basics/07strings.js
@@ -0,0 +1,14 @@
+const name = "Adarsh"
+const repocount = 350
+
+//console.log(`Hello my name is ${name} and my repo count is ${repocount}`)
+
+const gameName = new String('adak')
+
+// console.log(gameName[0])
+// console.log(gameName.__proto__)
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+console.log(gameName.charAt(5))
+console.log(gameName.indexOf('a'))
+
